e8ca9f96b76ac122a91f42a627bba5efaf561384
Augusto Petraglia <augustopetraglia@gmail.com>
2021-03-17 00:40:12 -0300

    add dinamyc routes for product pages

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 import products from '../products.json'
 import { useCart } from '../hooks/use-cart'
@@ -23,33 +24,27 @@ export default function Home() {
         <p className={styles.description}>
           Just another ecom store
         </p>
-
-        {/* <p className={styles.description}>
-          <strong>Items:</strong> {totalItems}
-          <br />
-          <strong>Total Cost:</strong> ${subTotal}
-          <br />
-          <button className={styles.button} onClick={checkout}>Check Out</button>
-        </p> */}
-
+        
         <ul className={styles.grid}>
           {products.map(product => {
             const { id, title, price, description, image } = product;
             return(
               <li key={id} className={styles.card}>
-                <a href="https://nextjs.org/docs">
-                  <img src={image} alt={title} />
-                  <h3>{title}</h3>
-                  <p>${price}</p>
-                  <p>{description}</p>
-                </a>
-                <p>
-                  <button className={styles.button} onClick={() => {
-                    addToCart({
-                      id
-                    })
-                  }}>Add To Cart</button>
-                </p>
+                <Link href={`/products/${id}`}>
+                  <a href="https://nextjs.org/docs">
+                    <img src={image} alt={title} />
+                    <h3>{title}</h3>
+                    <p>${price}</p>
+                    <p>{description}</p>
+                  </a>
+                </Link>
+                  <p>
+                    <button className={styles.button} onClick={() => {
+                      addToCart({
+                        id
+                      })
+                    }}>Add To Cart</button>
+                  </p>
               </li>
             )
           })}
